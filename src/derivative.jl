014353e4bd1022c701e5e38a9d72836da705a927
Songchen Tan <songchen@mit.edu>
2022-12-01 21:15:43 +0000

    Add some adjoints

--- src/derivative.jl
+++ src/derivative.jl
@@ -2,13 +2,14 @@
 export derivative
 
 function derivative(f::Function, x::T, order::Int64) where {T<:Number}
-    m = TaylorScalar(x, one(x), zeros(T, order - 1)...)
-    result = f(m)
+    t = TaylorScalar(x, one(x), zeros(T, order - 1)...)
+    result = f(t)
     return getindex(value(result), order + 1)
 end
 
 function derivative(f::Function, x::Vector{T}, index::Int64, order::Int64) where {T<:Number}
-    nx = [TaylorScalar(val, ind == index ? one(T) : zero(T), zeros(T, order - 1)...) for (ind, val) in enumerate(x)]
-    result = f(nx)
+    pertub = [ind == index ? one(T) : zero(T) for ind in eachindex(x)]
+    t = TaylorVector(x, pertub, [zeros(T, length(x)) for i in 1:(order - 1)]...)
+    result = f(t)
     return getindex(value(result), order + 1)
 end
