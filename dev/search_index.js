c6628497c808bc1a31925392e56253e47c0cad13
Documenter.jl <documenter@juliadocs.github.io>
2022-12-16 18:04:28 +0000

    build based on 5a28457

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TaylorDiff","category":"page"},{"location":"#TaylorDiff","page":"Home","title":"TaylorDiff","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TaylorDiff.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TaylorDiff]","category":"page"}]
+}
