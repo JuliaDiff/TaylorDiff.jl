dfe1e4b9b4d68cf4ab39e7bb10edcd0762fa8cad
Songchen Tan <i@tansongchen.com>
2022-12-23 09:56:27 -0500

    Remove convert

--- src/derivative.jl
+++ src/derivative.jl
@@ -2,34 +2,34 @@
 export derivative
 
 """
-    derivative(f::Function, x::T, order::Int64)
-    derivative(f::F, x::T, ::Val{N})
+    derivative(f, x::T, order::Int64)
+    derivative(f, x::T, ::Val{N})
 
 Computes `order`-th derivative of `f` w.r.t. `x`.
 
-    derivative(f::Function, x::Vector{T}, l::Vector{T}, order::Int64)
-    derivative(f::F, x::Vector{T}, l::Vector{T}, ::Val{N})
+    derivative(f, x::Vector{T}, l::Vector{T}, order::Int64)
+    derivative(f, x::Vector{T}, l::Vector{T}, ::Val{N})
 
 Computes `order`-th directional derivative of `f` w.r.t. `x` in direction `l`.
 """
 function derivative end
 
-@inline function derivative(f::Function, x::T, order::Int64) where {T <: Number}
+@inline function derivative(f, x::T, order::Int64) where {T <: Number}
     derivative(f, x, Val{order + 1}())
 end
 
-@inline function derivative(f::Function, x::Vector{T}, l::Vector{T},
+@inline function derivative(f, x::Vector{T}, l::Vector{T},
                             order::Int64) where {T <: Number}
     derivative(f, x, l, Val{order + 1}())
 end
 
-@inline function derivative(f::F, x::T, ::Val{N}) where {F, T <: Number, N}
-    t = TaylorScalar{T, N}(x)
+@inline function derivative(f, x::T, ::Val{N}) where {T <: Number, N}
+    t = TaylorScalar{T, N}(x, one(x))
     return extract_derivative(f(t), N)
 end
 
-@inline function derivative(f::F, x::Vector{T}, l::Vector{T},
-                            ::Val{N}) where {F, T <: Number, N}
+@inline function derivative(f, x::Vector{T}, l::Vector{T},
+                            ::Val{N}) where {T <: Number, N}
     t = map(TaylorScalar{T, N}, x, l)
     return extract_derivative(f(t), N)
 end
