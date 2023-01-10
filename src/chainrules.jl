6b34351b9ef9a2a8f7b3125b88def7b95379f063
Songchen Tan <i@tansongchen.com>
2023-01-10 12:22:40 -0500

    Add rrule for gemv

--- src/chainrules.jl
+++ src/chainrules.jl
@@ -1,6 +1,5 @@
 import ChainRulesCore: rrule, RuleConfig
 
-@opt_out rrule(::Any, ::TaylorScalar)
 @opt_out rrule(::Any, ::TaylorScalar, ::TaylorScalar)
 @opt_out rrule(::Any, ::TaylorScalar, ::Any)
 @opt_out rrule(::typeof(*), ::TaylorScalar, ::TaylorScalar)
@@ -27,4 +26,19 @@ function rrule(::typeof(extract_derivative), t::TaylorScalar{T, N},
         NoTangent(), TaylorScalar((zeros(T, i - 1)..., d̄, zeros(T, N - i)...)), NoTangent()
     end
     return extract_derivative(t, i), extract_derivative_pullback
+end
+
+function rrule(::typeof(*), A::Matrix{T}, t::Vector{TaylorScalar{T, N}}) where {N, T <: Number}
+    gemv_pullback(x̄) = NoTangent(), map(primal, x̄) * transpose(map(primal, t)), transpose(A) * x̄
+    return A * t, gemv_pullback
+end
+
+function rrule(::typeof(+), v::Vector{T}, t::Vector{TaylorScalar{T, N}}) where {N, T <: Number}
+    vadd_pullback(x̄) = NoTangent(), map(primal, x̄), x̄
+    return v + t, vadd_pullback
+end
+
+function rrule(::typeof(+), t::Vector{TaylorScalar{T, N}}, v::Vector{T}) where {N, T <: Number}
+    vadd_pullback(x̄) = NoTangent(), x̄, map(primal, x̄)
+    return t + v, vadd_pullback
 end
