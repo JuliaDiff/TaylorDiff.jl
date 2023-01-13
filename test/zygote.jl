72c28c47a962f1efdace91d24ae21c0d1ba11c6f
Songchen Tan <i@tansongchen.com>
2023-01-13 16:22:36 -0500

    Improve coverage

--- test/zygote.jl
+++ test/zygote.jl
@@ -14,12 +14,14 @@ using Zygote
 end
 
 @testset "Zygote for parameter optimization" begin
-    linear_model(x, p) = exp.(p * x)[1]
-    some_x, some_v, some_p = [0.58, 0.36], [0.23, 0.11], [0.49 0.96]
-    loss_taylor(p) = derivative(x -> linear_model(x, p), some_x, some_v, 1)
+    gradient(p -> derivative(x -> sum(exp.(x + p)), [1.0, 1.0], [1.0, 0.0], 1), [0.5, 0.7])
+    gradient(p -> derivative(x -> sum(exp.(p + x)), [1.0, 1.0], [1.0, 0.0], 1), [0.5, 0.7])
+    linear_model(x, p, b) = exp.(b + p * x + b)[1]
+    some_x, some_v, some_p, some_b = [0.58, 0.36], [0.23, 0.11], [0.49 0.96], [0.88]
+    loss_taylor(p) = derivative(x -> linear_model(x, p, some_b), some_x, some_v, 1)
     ε = cbrt(eps(Float64))
     loss_finite(p) =
-        let f = x -> linear_model(x, p)
+        let f = x -> linear_model(x, p, some_b)
             (f(some_x + ε * some_v) - f(some_x - ε * some_v)) / 2ε
         end
     @test gradient(loss_taylor, some_p)[1] ≈ gradient(loss_finite, some_p)[1]
