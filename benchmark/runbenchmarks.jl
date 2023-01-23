8c38fda4edf544e05ba4d97e9ddd5ced22d24a2f
Songchen Tan <i@tansongchen.com>
2023-01-23 17:54:29 +0000

    Remove branch info

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -7,7 +7,6 @@ using BenchmarkTools, PkgBenchmark
 using BenchmarkTools: Trial, TrialEstimate, Parameters
 import JSON: lower, json
 using HTTP: put
-using LibGit2: GitRepo, headname
 
 dict(x) = Dict(name => lower(getfield(x, name)) for name in fieldnames(typeof(x)))
 
@@ -18,10 +17,7 @@ lower(estimate::TrialEstimate) = dict(estimate)
 lower(parameters::Parameters) = dict(parameters)
 
 function benchmark()
-    repo = GitRepo(pwd())
-    branch = headname(repo)
-    config = BenchmarkConfig(id = branch)
-    results = benchmarkpkg(TaylorDiff, config)
+    results = benchmarkpkg(TaylorDiff)
     endpoint = "https://benchmark.tansongchen.com"
     put(endpoint; body = json(results))
 end
