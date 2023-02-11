50f38aeaf0a12656fd5f846066fff73147536052
Songchen Tan <i@tansongchen.com>
2023-02-10 20:14:13 -0500

    Fix CI script

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -6,6 +6,7 @@ using TaylorDiff
 using BenchmarkTools, PkgBenchmark
 using BenchmarkTools: Trial, TrialEstimate, Parameters
 import JSON: lower, json
+using Dates
 using HTTP: put
 
 dict(x) = Dict(name => lower(getfield(x, name)) for name in fieldnames(typeof(x)))
@@ -16,10 +17,33 @@ lower(trial::Trial) = lower(median(trial))
 lower(estimate::TrialEstimate) = dict(estimate)
 lower(parameters::Parameters) = dict(parameters)
 
-function benchmark()
-    results = benchmarkpkg(TaylorDiff)
-    endpoint = "https://benchmark.tansongchen.com"
-    put(endpoint; body = json(results))
+struct BenchmarkContext
+    datetime::DateTime
+    commit::String
+    branch::String
+    tag::String
 end
 
-benchmark()
+lower(context::BenchmarkContext) = dict(context)
+
+abstract type ContextProvider end
+
+struct Dummy <: ContextProvider end
+provide_context(::Dummy) = BenchmarkContext(now(), "", "", "")
+
+struct Buildkite <: ContextProvider end
+function provide_context(::Buildkite)
+    getenv(name::String) = String(strip(ENV[name]))
+    BenchmarkContext(now(), # datetime
+                     getenv("BUILDKITE_COMMIT"), # commit
+                     getenv("BUILDKITE_BRANCH"), # branch
+                     getenv("BUILDKITE_TAG"))
+end
+
+context = provide_context(Buildkite())
+results = benchmarkpkg(TaylorDiff)
+(; benchmarkgroup, benchmarkconfig) = results
+reconstructed = Dict("suite" => benchmarkgroup,
+                     "context" => context,
+                     "config" => benchmarkconfig)
+put("https://benchmark.tansongchen.com/TaylorDiff.jl"; body = json(reconstructed))
