a02f8bf12ec4d0acf73474766949359308dade2e
Matthieu Barreau <matth.barreau@gmail.com>
2023-08-23 15:48:50 +0200

    TaylorDiff deals with matrix

--- src/derivative.jl
+++ src/derivative.jl
@@ -1,4 +1,5 @@
 
+using SliceMap
 export derivative
 
 """
@@ -18,14 +19,29 @@ function derivative end
     derivative(f, x, Val{order + 1}())
 end
 
+
+@inline function derivative(f, x::M, order::Int64) where {M <: AbstractMatrix{<:Number}}
+    mapcols(u -> derivative(f, u[1], order), x)
+end
+
 @inline function derivative(f, x::AbstractVector{T}, l::AbstractVector{S},
     order::Int64) where {T <: Number, S <: Number}
     derivative(f, x, l, Val{order + 1}())
 end
 
+@inline function derivative(f, x::M, l::V,
+    order::Int64) where {M <: AbstractMatrix{<:Number}, V <: AbstractVector{<:Number}}
+    @info "test"
+    mapcols(u -> derivative(f, u, l, order), x)
+end
+
 @inline function derivative(f, x::T, ::Val{N}) where {T <: Number, N}
     t = TaylorScalar{T, N}(x, one(x))
     return extract_derivative(f(t), N)
+end
+
+@inline function derivative(f, x::M, ::Val{N}) where {M <: AbstractMatrix{<:Number}, N}
+    mapcols(u -> derivative(f, u[1], N), x)
 end
 
 # Need to rewrite like this to help Zygote infer types
@@ -35,4 +51,9 @@ make_taylor(t0::T, t1::S, ::Val{N}) where {T, S, N} = TaylorScalar{T, N}(t0, T(t
     vN::Val{N}) where {T <: Number, S <: Number, N}
     t = map((t0, t1) -> make_taylor(t0, t1, vN), x, l) # i.e. map(TaylorScalar{T, N}, x, l)
     return extract_derivative(f(t), N)
+end
+
+@inline function derivative(f, x::M, l::V,
+    vN::Val{N}) where {M <: AbstractMatrix{<:Number}, V <: AbstractVector{<:Number}, N}
+    mapcols(u -> derivative(f, u, l, vN), x)
 end
