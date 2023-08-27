d88896e5ae575444d65752c4cdc7ed59ac90bedb
Matthieu Barreau <matth.barreau@gmail.com>
2023-08-27 18:47:51 +0200

    Add tests to matrix differentiation

--- test/zygote.jl
+++ test/zygote.jl
@@ -1,16 +1,33 @@
-using Zygote
+using Zygote, LinearAlgebra
 
 @testset "Zygote for mixed derivative" begin
     some_number = 0.7
+    some_numbers = [0.3 0.4 2.0;]
     for f in (exp, log, sqrt, sin, asin, sinh, asinh)
         @test gradient(x -> derivative(f, x, 2), some_number)[1] ≈
               derivative(f, some_number, 3)
+        derivative_result = vec(derivative(f, some_numbers, 3))
+        @test Zygote.jacobian(x -> derivative(f, x, 2), some_numbers)[1] ≈
+              diagm(derivative_result)
     end
+
+    some_matrix = [0.7; 0.1;; 0.4; 0.2]
+    f = x -> sum(tanh.(x), dims = 1)
+    dfdx1(m, x) = derivative(u -> sum(m(u)), x, [1.0, 0.0], 1)
+    dfdx2(m, x) = derivative(u -> sum(m(u)), x, [0.0, 1.0], 1)
+    res(m, x) = dfdx1(m, x) .+ 2 * dfdx2(m, x)
+    grads = Zygote.gradient(some_matrix) do x
+        sum(res(f, x))
+    end
+    expected_grads = x -> -2 * sinh(x) / cosh(x)^3
+    @test grads[1] ≈ [1 0; 0 2] * expected_grads.(some_matrix)
+
     @test gradient(x -> derivative(x -> x * x, x, 1),
         5.0)[1] ≈ 2.0
 
     g(x) = x[1] * x[1] + x[2] * x[2]
-    @test gradient(x -> derivative(g, x, [1.0, 0.0], 1), [1.0, 2.0])[1] ≈ [2.0, 0.0]
+    @test gradient(x -> derivative(g, x, [1.0, 0.0], 1),
+        [1.0, 2.0])[1] ≈ [2.0, 0.0]
 end
 
 @testset "Zygote for parameter optimization" begin
