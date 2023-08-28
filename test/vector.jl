73fe0be077f6dc1b1fd100c6dd507f951ac1ca7d
Matthieu Barreau <matth.barreau@gmail.com>
2023-08-28 13:40:38 +0200

    Fix broken tests

--- test/vector.jl
+++ test/vector.jl
@@ -2,5 +2,5 @@
 @testset "Vector" begin
     g(x) = x[1] * x[1] + x[2] * x[2]
     @test derivative(g, [1.0, 2.0], [1.0, 0.0], 1) ≈ 2.0
-    @test derivative(g, [1.0; 2.0;; 2.0; 3.0], [1.0, 1.0], 1) ≈ [6.0 10.0]
+    @test derivative(g, [1.0 2.0; 2.0 3.0], [1.0, 1.0], 1) ≈ [6.0 10.0]
 end
