7011b6ac906257275a21267ae5838434a97f8bf6
Songchen Tan <i@tansongchen.com>
2024-10-04 15:18:55 -0400

    Add broadcasting

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using TaylorDiff
 using Test
 
-# include("primitive.jl")
-# include("derivative.jl")
+include("primitive.jl")
+include("derivative.jl")
 include("downstream.jl")
 # include("lux.jl")
