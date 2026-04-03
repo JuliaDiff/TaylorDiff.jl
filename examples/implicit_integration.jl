86fed737fdfefa2d466b229d749f4342c99aeb0a
Songchen Tan <i@tansongchen.com>
2026-04-03 19:39:51 -0400

    Add Taylor-Padé integrators

--- examples/implicit_integration.jl
+++ examples/implicit_integration.jl
@@ -1,244 +1,78 @@
 include("integration.jl")
 
-struct ImplicitFirstOrder{T}
-    μ::T
-end
+using NonlinearSolve, ODEProblemLibrary, LinearAlgebra
 
-function (solver::ImplicitFirstOrder)(prob; h)
-    μ = solver.μ
-    fast_oop, _ = build_jetcoeffs(prob.f, prob.p, Val(1), length(prob.u0))
+# a simple fixed time-step driver for rapid algorithm prototyping
+function integrate(prob, solver; h)
+    cache = prepare(prob, solver)
     t0, t_end = prob.tspan
     t = t0
     u = copy(prob.u0)
     while t < t_end
-        nlf = (u_next, u_curr) -> begin
-            du = get_coefficient(fast_oop(u_next, t + μ * h), 1)
-            u_next .- u_curr .- μ * h .* du
-        end
-        guess = copy(u)
-        nlprob = NonlinearProblem(nlf, guess, u)
-        sol = solve(nlprob)
-        f = get_coefficient(fast_oop(sol.u, t + μ * h), 1)
-        u .= sol.u + (1 - μ) * h * f
+        update!(u, prob, solver, cache, t, h)
         t += h
     end
     return u
 end
 
-struct ImplicitSecondOrder{T}
+# the μ-Taylor solver in NASA paper
+struct MuTaylor{P, T}
+    order::Val{P}
     μ::T
 end
 
-function (solver::ImplicitSecondOrder)(prob; h)
-    μ = solver.μ
-    fast_oop, _ = build_jetcoeffs(prob.f, prob.p, Val(2), length(prob.u0))
-    t0, t_end = prob.tspan
-    t = t0
-    u = copy(prob.u0)
-    while t < t_end
-        nlf = (u_next, u_curr) -> begin
-            poly = fast_oop(u_next, t + μ * h)
-            u1 = get_coefficient(poly, 1)
-            u2 = get_coefficient(poly, 2)
-            u_curr + μ * h * u1 - (μ * h)^2 * u2 - u_next
-        end
-        guess = copy(u)
-        nlprob = NonlinearProblem(nlf, guess, u)
-        sol = solve(nlprob)
-        poly = fast_oop(sol.u, t + μ * h)
-        u1 = get_coefficient(poly, 1)
-        u2 = get_coefficient(poly, 2)
-        u .= real(sol.u + (1 - μ) * h * u1 + ((1 - μ) * h)^2 * u2)
-        t += h
-    end
-    return u
+function prepare(prob, solver::MuTaylor{P, T}) where {T, P}
+    propagator_all, _ = build_propagator(prob.f, prob.p, ntuple(_ -> 1.0, solver.order), length(prob.u0))
+    propagator, _ = propagator_all
+    return (; propagator)
 end
 
-struct ImplicitThirdOrder{T}
-    μ::T
+function update!(u, prob, solver::MuTaylor{P, T}, cache, t, h) where {T, P}
+    (; μ) = solver
+    (; propagator) = cache
+    tm = t + μ * h
+    nlf = (u_next, u_curr) -> propagator(u_next, tm, -μ * h) .- u_curr
+    nlprob = NonlinearProblem(nlf, copy(u), u)
+    sol = solve(nlprob)
+    return u .= real(propagator(sol.u, tm, (1 - μ) * h))
 end
 
-function (solver::ImplicitThirdOrder)(prob; h)
-    μ = solver.μ
-    uh = μ * h
-    cuh = (1 - μ) * h
-    fast_oop, _ = build_jetcoeffs(prob.f, prob.p, Val(3), length(prob.u0))
-    t0, t_end = prob.tspan
-    t = t0
-    u = copy(prob.u0)
-    while t < t_end
-        nlf = (u_next, u_curr) -> begin
-            poly = fast_oop(u_next, t + uh)
-            u1 = get_coefficient(poly, 1)
-            u2 = get_coefficient(poly, 2)
-            u3 = get_coefficient(poly, 3)
-            u_curr + uh * u1 - uh^2 * u2 + uh^3 * u3 - u_next
-        end
-        guess = copy(u)
-        nlprob = NonlinearProblem(nlf, guess, u)
-        sol = solve(nlprob)
-        poly = fast_oop(sol.u, t + uh)
-        u1 = get_coefficient(poly, 1)
-        u2 = get_coefficient(poly, 2)
-        u3 = get_coefficient(poly, 3)
-        u .= sol.u + cuh * u1 + cuh^2 * u2 + cuh^3 * u3
-        t += h
-    end
-    return u
-end
-
-struct ImplicitThirdOrderExtrapolated{T1, T2}
+struct MuTaylorExtrapolation{P, T1, T2, T3, T4}
+    order::Val{P}
     μ1::T1
-    c1::T1
-    μ2::T2
-    c2::T1
+    c1::T2
+    μ2::T3
+    c2::T4
 end
 
-function (solver::ImplicitThirdOrderExtrapolated)(prob; h)
-    uh1 = solver.μ1 * h
-    cuh1 = (1 - solver.μ1) * h
-    uh2 = solver.μ2 * h
-    cuh2 = (1 - solver.μ2) * h
-    fast_oop, _ = build_jetcoeffs(prob.f, prob.p, Val(3), length(prob.u0))
-    t0, t_end = prob.tspan
-    t = t0
-    u = copy(prob.u0)
-    while t < t_end
-        nlf = (u_next, u_curr) -> begin
-            poly = fast_oop(u_next, t + uh1)
-            u1 = get_coefficient(poly, 1)
-            u2 = get_coefficient(poly, 2)
-            u3 = get_coefficient(poly, 3)
-            u_curr + uh1 * u1 - uh1^2 * u2 + uh1^3 * u3 - u_next
-        end
-        guess = copy(u)
-        nlprob = NonlinearProblem(nlf, guess, u)
-        sol = solve(nlprob)
-        poly = fast_oop(sol.u, t + uh1)
-        u1 = get_coefficient(poly, 1)
-        u2 = get_coefficient(poly, 2)
-        u3 = get_coefficient(poly, 3)
-        u_trial1 = real(sol.u + cuh1 * u1 + cuh1^2 * u2 + cuh1^3 * u3)
-        nlf2 = (u_next, u_curr) -> begin
-            poly = fast_oop(u_next, t + uh2)
-            u1 = get_coefficient(poly, 1)
-            u2 = get_coefficient(poly, 2)
-            u3 = get_coefficient(poly, 3)
-            u_curr + uh2 * u1 - uh2^2 * u2 + uh2^3 * u3 - u_next
-        end
-        guess2 = copy(u)
-        nlprob2 = NonlinearProblem(nlf2, guess2, u)
-        sol2 = solve(nlprob2)
-        poly2 = fast_oop(sol2.u, t + uh2)
-        u1_2 = get_coefficient(poly2, 1)
-        u2_2 = get_coefficient(poly2, 2)
-        u3_2 = get_coefficient(poly2, 3)
-        u_trial2 = real(sol2.u + cuh2 * u1_2 + cuh2^2 * u2_2 + cuh2^3 * u3_2)
-        u .= (0.8 * u_trial1 + 0.2 * u_trial2)
-        t += h
-    end
-    return u
+function prepare(prob, solver::MuTaylorExtrapolation{P, T1, T2, T3, T4}) where {P, T1, T2, T3, T4}
+    propagator_all, _ = build_propagator(prob.f, prob.p, ntuple(_ -> 1.0, solver.order), length(prob.u0))
+    propagator, _ = propagator_all
+    return (; propagator)
 end
 
-struct ImplicitFourthOrder{T}
-    μ::T
+function update!(u, prob, solver::MuTaylorExtrapolation{P, T1, T2, T3, T4}, cache, t, h) where {T1, T2, T3, T4, P}
+    (; μ1, c1, μ2, c2) = solver
+    (; propagator) = cache
+    tm1 = t + μ1 * h
+    tm2 = t + μ2 * h
+    nlf = (u_next, u_curr) -> propagator(u_next, tm1, -μ1 * h) - u_curr
+    nlprob1 = NonlinearProblem(nlf, copy(u), u)
+    sol1 = solve(nlprob1)
+    u_trial1 = real(propagator(sol1.u, tm1, (1 - μ1) * h))
+    nlf2 = (u_next, u_curr) -> propagator(u_next, tm2, -μ2 * h) - u_curr
+    nlprob2 = NonlinearProblem(nlf2, copy(u), u)
+    sol2 = solve(nlprob2)
+    u_trial2 = real(propagator(sol2.u, tm2, (1 - μ2) * h))
+    return u .= (c1 * u_trial1 + c2 * u_trial2)
 end
 
-function (solver::ImplicitFourthOrder)(prob; h)
-    μ = solver.μ
-    uh = μ * h
-    cuh = (1 - μ) * h
-    fast_oop, _ = build_jetcoeffs(prob.f, prob.p, Val(4), length(prob.u0))
-    t0, t_end = prob.tspan
-    t = t0
-    u = copy(prob.u0)
-    while t < t_end
-        nlf = (u_next, u_curr) -> begin
-            poly = fast_oop(u_next, t + uh)
-            u1 = get_coefficient(poly, 1)
-            u2 = get_coefficient(poly, 2)
-            u3 = get_coefficient(poly, 3)
-            u4 = get_coefficient(poly, 4)
-            u_curr + uh * u1 - uh^2 * u2 + uh^3 * u3 - uh^4 * u4 - u_next
-        end
-        guess = copy(u)
-        nlprob = NonlinearProblem(nlf, guess, u)
-        sol = solve(nlprob)
-        poly = fast_oop(sol.u, t + uh)
-        u1 = get_coefficient(poly, 1)
-        u2 = get_coefficient(poly, 2)
-        u3 = get_coefficient(poly, 3)
-        u4 = get_coefficient(poly, 4)
-        u .= sol.u + cuh * u1 + cuh^2 * u2 + cuh^3 * u3 + cuh^4 * u4
-        t += h
-    end
-    return u
-end
-
-struct ImplicitFourthOrderExtrapolated{T1, T2}
-    μ1::T2
-    c1::T1
-    μ2::T2
-    c2::T1
-end
-
-function (solver::ImplicitFourthOrderExtrapolated)(prob; h)
-    uh1 = solver.μ1 * h
-    cuh1 = (1 - solver.μ1) * h
-    uh2 = solver.μ2 * h
-    cuh2 = (1 - solver.μ2) * h
-    fast_oop, _ = build_jetcoeffs(prob.f, prob.p, Val(4), length(prob.u0))
-    t0, t_end = prob.tspan
-    t = t0
-    u = copy(prob.u0)
-    while t < t_end
-        nlf = (u_next, u_curr) -> begin
-            poly = fast_oop(u_next, t + uh1)
-            u1 = get_coefficient(poly, 1)
-            u2 = get_coefficient(poly, 2)
-            u3 = get_coefficient(poly, 3)
-            u4 = get_coefficient(poly, 4)
-            u_curr + uh1 * u1 - uh1^2 * u2 + uh1^3 * u3 - uh1^4 * u4 - u_next
-        end
-        guess = copy(u)
-        nlprob = NonlinearProblem(nlf, guess, u)
-        sol = solve(nlprob)
-        poly = fast_oop(sol.u, t + uh1)
-        u1 = get_coefficient(poly, 1)
-        u2 = get_coefficient(poly, 2)
-        u3 = get_coefficient(poly, 3)
-        u4 = get_coefficient(poly, 4)
-        u_trial1 = real(sol.u + cuh1 * u1 + cuh1^2 * u2 + cuh1^3 * u3 + cuh1^4 * u4)
-        nlf2 = (u_next, u_curr) -> begin
-            poly = fast_oop(u_next, t + uh2)
-            u1 = get_coefficient(poly, 1)
-            u2 = get_coefficient(poly, 2)
-            u3 = get_coefficient(poly, 3)
-            u4 = get_coefficient(poly, 4)
-            u_curr + uh2 * u1 - uh2^2 * u2 + uh2^3 * u3 - uh2^4 * u4 - u_next
-        end
-        guess2 = copy(u)
-        nlprob2 = NonlinearProblem(nlf2, guess2, u)
-        sol2 = solve(nlprob2)
-        poly2 = fast_oop(sol2.u, t + uh2)
-        u1_2 = get_coefficient(poly2, 1)
-        u2_2 = get_coefficient(poly2, 2)
-        u3_2 = get_coefficient(poly2, 3)
-        u4_2 = get_coefficient(poly2, 4)
-        u_trial2 = real(sol2.u + cuh2 * u1_2 + cuh2^2 * u2_2 + cuh2^3 * u3_2 +
-                        cuh2^4 * u4_2)
-        u .= (solver.c1 * u_trial1 + solver.c2 * u_trial2) / (solver.c1 + solver.c2)
-        t += h
-    end
-    return u
-end
-
-function estimate_order(solver, prob; dt1 = 0.01, dt2 = 0.005)
+function estimate_order(solver, prob; dt1 = 1/12, dt2 = 1/16)
     ref = prob.f.analytic(prob.u0, prob.p, prob.tspan[2])
     println("Reference: ", ref)
-    u1 = solver(prob; h = dt1)
+    u1 = integrate(prob, solver; h = dt1)
     println("u1: ", u1)
-    u2 = solver(prob; h = dt2)
+    u2 = integrate(prob, solver; h = dt2)
     println("u2: ", u2)
     err1 = norm(u1 - ref)
     err2 = norm(u2 - ref)
@@ -246,33 +80,123 @@ function estimate_order(solver, prob; dt1 = 0.01, dt2 = 0.005)
     return order
 end
 
-init = rand(2)
+u0 = rand(2)
 linear_f = ODEFunction(
-    ODEProblemLibrary.f_2dlinear, analytic = ODEProblemLibrary.f_2dlinear_analytic)
-prob = ODEProblem(linear_f, init, (0.0, 1.0), 1.01)
+    ODEProblemLibrary.f_2dlinear, analytic = ODEProblemLibrary.f_2dlinear_analytic
+)
+prob = ODEProblem(linear_f, u0, (0.0, 1.0), 1.01)
 prob_complex = ODEProblem(
-    linear_f, complex(init), (0.0, 1.0), 1.01)
+    linear_f, complex(u0), (0.0, 1.0), 1.01
+)
 
-ImplicitEuler = ImplicitFirstOrder(1.0)
-ImplicitMidpoint = ImplicitFirstOrder(0.5)
-estimate_order(ImplicitEuler, prob)
-estimate_order(ImplicitMidpoint, prob)
+ImplicitTaylor = MuTaylor(Val(1), 1.0)
+ImplicitTaylorMidpoint = MuTaylor(Val(1), 0.5)
+estimate_order(ImplicitTaylor, prob)
+estimate_order(ImplicitTaylorMidpoint, prob)
 
-ImplicitTaylor2 = ImplicitSecondOrder(1.0)
-ImplicitTaylor2Midpoint = ImplicitSecondOrder(0.5)
-ImplicitTaylor2Complex = ImplicitSecondOrder(0.5 + sqrt(3) * im / 6)
+ImplicitTaylor2 = MuTaylor(Val(2), 1.0)
+ImplicitTaylor2Midpoint = MuTaylor(Val(2), 0.5)
+ImplicitTaylor2Complex = MuTaylor(Val(2), 0.5 + sqrt(3) * im / 6)
 estimate_order(ImplicitTaylor2, prob)
 estimate_order(ImplicitTaylor2Midpoint, prob)
 estimate_order(ImplicitTaylor2Complex, prob_complex)
 
-ImplicitTaylor3Midpoint = ImplicitThirdOrder(0.5)
-ImplicitTaylor3Extrapolated = ImplicitThirdOrderExtrapolated(0.5, 0.8, 0.5 + 0.5im, 0.2)
+ImplicitTaylor3Midpoint = MuTaylor(Val(3), 0.5)
+ImplicitTaylor3Extrapolated = MuTaylorExtrapolation(Val(3), 0.5, 0.8, 0.5 + 0.5im, 0.2)
 estimate_order(ImplicitTaylor3Midpoint, prob)
 estimate_order(ImplicitTaylor3Extrapolated, prob_complex; dt1 = 1 / 12, dt2 = 1 / 16)
 
-ImplicitTaylor4Midpoint = ImplicitFourthOrder(0.5)
-ImplicitTaylor4Extrapolated = ImplicitFourthOrderExtrapolated(
+ImplicitTaylor4Midpoint = MuTaylor(Val(4), 0.5)
+ImplicitTaylor4Extrapolated = MuTaylorExtrapolation(
+    Val(4),
     0.5 + 0.5im * tan(π / 10), tan(π / 10) * sec(π / 10)^5,
-    0.5 + 0.5im * tan(3π / 10), tan(3π / 10) * sec(3π / 10)^5)
+    0.5 + 0.5im * tan(3π / 10), tan(3π / 10) * sec(3π / 10)^5
+)
 estimate_order(ImplicitTaylor4Midpoint, prob)
-estimate_order(ImplicitTaylor4Extrapolated, prob_complex; dt1 = 1 / 9, dt2 = 1 / 12)
+estimate_order(ImplicitTaylor4Extrapolated, prob_complex; dt1 = 1 / 12, dt2 = 1 / 16)
+
+function normalized_pade(p::Int, q::Int)
+    RI = Rational{BigInt}
+    N = p + q
+    c = Vector{RI}(undef, N + 1)
+    c[1] = big(1) // big(1)
+    for k in 1:N
+        c[k + 1] = c[k] // big(k)
+    end
+    if q == 0
+        dres = RI[]
+    else
+        A = zeros(RI, q, q)
+        rhs = zeros(RI, q)
+        for i in 1:q
+            k = p + i                     # 对应 z^k 阶 (0-indexed)
+            rhs[i] = -c[k + 1]             # -c_k
+            for j in 1:q
+                idx = k - j               # 0-indexed Taylor index
+                A[i, j] = idx >= 0 ? c[idx + 1] : big(0) // big(1)
+            end
+        end
+        dres = A \ rhs                       # Rational 精确求解
+    end
+    n = zeros(RI, p + 1)
+    for k in 0:p
+        n[k + 1] = c[k + 1]
+        for j in 1:min(k, q)
+            n[k + 1] += dres[j] * c[k - j + 1]
+        end
+    end
+    d = vcat(big(1) // big(1), dres)
+    normalized_n = [x * factorial(k - 1) for (k, x) in enumerate(n)]
+    normalized_d = [x * factorial(k - 1) for (k, x) in enumerate(d)]
+    return normalized_n, normalized_d
+end
+
+# the μ-Taylor solver in NASA paper
+struct TaylorPade{P, Q}
+    n_order::Val{P}
+    d_order::Val{Q}
+end
+
+function prepare(prob, solver::TaylorPade{P, Q}) where {P, Q}
+    polynomial_p, polynomial_q = normalized_pade(P, Q)
+    tuple_p = Base.tail(tuple(map(Float64, polynomial_p)...))
+    tuple_q = Base.tail(tuple(map(Float64, polynomial_q)...))
+    println("Pade coefficients (numerator): ", tuple_p)
+    println("Pade coefficients (denominator): ", tuple_q)
+    propagator_p_all, _ = build_propagator(prob.f, prob.p, tuple_p, length(prob.u0))
+    propagator_q_all, _ = build_propagator(prob.f, prob.p, tuple_q, length(prob.u0))
+    propagator_p, _ = propagator_p_all
+    propagator_q, _ = propagator_q_all
+    return (; propagator_p, propagator_q)
+end
+
+function update!(u, prob, solver::TaylorPade{P, Q}, cache, t, h) where {P, Q}
+    (; propagator_p, propagator_q) = cache
+    rhs = propagator_p(u, t, h)
+    nlf = (u_next, rhs) -> propagator_q(u_next, t + h, h) .- rhs
+    nlprob = NonlinearProblem(nlf, copy(u), rhs)
+    sol = solve(nlprob)
+    return u .= sol.u
+end
+
+# diagonal: Taylor-Gauss solvers
+TaylorPade1v1 = TaylorPade(Val(1), Val(1))
+TaylorPade2v2 = TaylorPade(Val(2), Val(2))
+TaylorPade3v3 = TaylorPade(Val(3), Val(3))
+estimate_order(TaylorPade1v1, prob)
+estimate_order(TaylorPade2v2, prob)
+estimate_order(TaylorPade3v3, prob)
+
+# sub-diagonal: Taylor-Radau solvers
+TaylorPade0v1 = TaylorPade(Val(0), Val(1))
+TaylorPade1v2 = TaylorPade(Val(1), Val(2))
+TaylorPade2v3 = TaylorPade(Val(2), Val(3))
+estimate_order(TaylorPade0v1, prob)
+estimate_order(TaylorPade1v2, prob)
+estimate_order(TaylorPade2v3, prob)
+
+# sub-sub-diagonal: Taylor-Lobatto solvers
+TaylorPade0v2 = TaylorPade(Val(0), Val(2))
+TaylorPade1v3 = TaylorPade(Val(1), Val(3))
+estimate_order(TaylorPade0v2, prob)
+estimate_order(TaylorPade1v3, prob)
--- examples/integration.jl
+++ examples/integration.jl
@@ -1,15 +1,11 @@
 using TaylorDiff: TaylorDiff, TaylorScalar, make_seed, flatten, get_coefficient,
-                  set_coefficient, append_coefficient
-using TaylorSeries, TaylorIntegration
-using LinearAlgebra
-using ODEProblemLibrary, OrdinaryDiffEq, BenchmarkTools, Symbolics
-using CairoMakie
-
-SymbolicUtils.ENABLE_HASHCONSING[] = true
+    set_coefficient, append_coefficient
+using OrdinaryDiffEq, Symbolics
+using SciMLBase: unwrapped_f
 
 # There are two ways to compute the Taylor coefficients of a ODE solution
 # 1. Using naive repeated differentiation
-# 2. First simplify the expansion using Symbolics and then evaluate the expression
+# 2. Using Symbolics to fuse various orders of differentiation together
 
 """
 # The first method
@@ -29,10 +25,10 @@ function jetcoeffs(f::ODEFunction{iip}, u0, p, t0, ::Val{P}) where {P, iip}
         d = get_coefficient(fu, index - 1) / index
         u = set_coefficient(u, index, d)
     end
-    u
+    return u
 end
 
-function jetcoeffs_inplace!(u, fu, f::ODEFunction{true}, u0, p, t0, ::Val{P}) where {P}
+function jetcoeffs!(u, fu, f::ODEFunction{true}, u0, p, t0, ::Val{P}) where {P}
     t = TaylorScalar{P}(t0, one(t0))
     for index in 1:P
         if iip
@@ -43,41 +39,7 @@ function jetcoeffs_inplace!(u, fu, f::ODEFunction{true}, u0, p, t0, ::Val{P}) wh
         d = get_coefficient(fu, index - 1) / index
         u = set_coefficient(u, index, d)
     end
-    u
-end
-
-function scalar_test()
-    P = 6
-    prob = prob_ode_linear
-    t0 = prob.tspan[1]
-    # TaylorIntegration test
-    ts = t0 + Taylor1(typeof(t0), P)
-    u_ts = Taylor1(prob.u0, P)
-    @btime TaylorIntegration.jetcoeffs!($prob.f, $ts, $u_ts, $prob.p)
-
-    # TaylorDiff test
-    @btime jetcoeffs($prob.f, $prob.u0, $prob.p, $t0, Val($P))
-    u_td = jetcoeffs(prob.f, prob.u0, prob.p, t0, Val(P))
-    @assert u_ts.coeffs ≈ collect(flatten(u_td))
-end
-
-function array_test()
-    P = 6
-    prob = prob_ode_lotkavolterra
-    t0 = prob.tspan[1]
-    # TaylorIntegration test
-    ts = t0 + Taylor1(typeof(t0), P)
-    u_ts = [Taylor1(x, P) for x in prob.u0]
-    du = similar(u_ts)
-    uaux = similar(u_ts)
-    @btime TaylorIntegration.jetcoeffs!($prob.f, $ts, $u_ts, $du, $uaux, $prob.p)
-
-    # TaylorDiff test
-    @btime jetcoeffs($prob.f, $prob.u0, $prob.p, $t0, Val($P))
-    u_td = jetcoeffs(prob.f, prob.u0, prob.p, t0, Val(P))
-    for i in eachindex(u_ts)
-        @assert u_ts[i].coeffs ≈ collect(flatten(u_td[i]))
-    end
+    return u
 end
 
 """
@@ -85,7 +47,17 @@ end
 
 For ODE u' = f(u, p, t) and initial condition (u0, t0), symbolically computes Taylor expansion of the solution `u` up to order `P`, and then builds a function to evaluate the expression.
 """
-function build_jetcoeffs(f::ODEFunction{iip}, p, ::Val{P}, length = nothing) where {P, iip}
+function build_jet(f::ODEFunction{iip}, p, order, length = nothing) where {iip}
+    f = unwrapped_f(f)
+    return build_jet(f, Val{iip}(), p, order, length)
+end
+
+function build_jet(f, ::Val{iip}, p, order::Val{P}, length = nothing) where {P, iip}
+    if haskey(JET_CACHE, f)
+        list = JET_CACHE[f]
+        index = findfirst(x -> x[1] == order && x[2] == p, list)
+        index !== nothing && return list[index][3]
+    end
     @variables t0::Real
     u0 = isnothing(length) ? Symbolics.variable(:u0) : Symbolics.variables(:u0, 1:length)
     if iip
@@ -107,157 +79,83 @@ function build_jetcoeffs(f::ODEFunction{iip}, p, ::Val{P}, length = nothing) whe
         d = get_coefficient(fu, index - 1) / index
         u = append_coefficient(u, d)
     end
-    u_term = make_term.(u)
-    build_function(u_term, u0, t0; expression = Val(false), cse = true)
-end
 
-function make_term(a)
-    term(TaylorScalar, Symbolics.unwrap(a.value), map(Symbolics.unwrap, a.partials))
-end
-
-function simplify_scalar_test()
-    P = 6
-    prob = prob_ode_linear
-    t0 = prob.tspan[1]
-    @btime jetcoeffs($prob.f, $prob.u0, $prob.p, $t0, Val($P))
-    fast_jetcoeffs = build_jetcoeffs(prob.f, prob.p, Val(P))
-    @btime $fast_jetcoeffs($prob.u0, $t0)
-end
-
-function simplify_array_test()
-    P = 6
-    prob = prob_ode_lotkavolterra
-    t0 = prob.tspan[1]
-    @btime jetcoeffs($prob.f, $prob.u0, $prob.p, $t0, Val($P))
-    fast_oop, fast_iip = build_jetcoeffs(prob.f, prob.p, Val(P), length(prob.u0))
-    @btime $fast_oop($prob.u0, $t0)
-end
-
-@generated function evaluate_polynomial(t::TaylorScalar{T, P}, z) where {T, P}
-    ex = :(v[$(P + 1)])
-    for i in P:-1:1
-        ex = :(v[$i] + z * $ex)
-    end
-    return :($(Expr(:meta, :inline)); v = flatten(t); $ex)
-end
-
-function plot_simplification_effect()
-    prob = ODEProblem(pcr3bp!, q0, tspan, p)
-    t0 = prob.tspan[1]
-    raw = Float64[]
-    optimized = Float64[]
-    orders = [4, 6, 8, 10, 12]
-    for P in orders
-        raw_time = @belapsed jetcoeffs($prob.f, $prob.u0, $prob.p, $t0, Val($P))
-        fast_oop, fast_iip = build_jetcoeffs(prob.f, prob.p, Val(P), length(prob.u0))
-        optimized_time = @belapsed $fast_oop($prob.u0, $t0)
-        push!(raw, raw_time)
-        push!(optimized, optimized_time)
-    end
-
-    colors = Makie.wong_colors()
-    f = begin
-        f = Figure(resolution = (700, 400))
-        ax = Axis(f[1, 1],
-            xlabel = "Order",
-            ylabel = "Time for computing Taylor polynomial (s)",
-            title = "Effect of Symbolic Simplification on Computing Taylor Polynomial",
-            xticks = orders,
-            yscale = log10
+    # Flatten TaylorScalar coefficients for build_function (it doesn't handle custom structs)
+    # Then wrap result back into TaylorScalar
+    if u isa TaylorScalar
+        # Scalar case: build function for array of coefficients
+        coeffs = collect(TaylorDiff.flatten(u))
+        jet_coeffs = build_function(coeffs, u0, t0; expression = Val(false), cse = true)
+        # Wrap to return TaylorScalar
+        jet = (u0_val, t0_val) -> TaylorScalar(Tuple(jet_coeffs[1](u0_val, t0_val)))
+    elseif u isa AbstractArray && eltype(u) <: TaylorScalar
+        # Array case: build function for matrix of coefficients
+        # Each row is the coefficients of one TaylorScalar
+        n = Base.length(u)
+        coeffs_matrix = [TaylorDiff.flatten(u[i])[j] for i in 1:n, j in 1:(P + 1)]
+        jet_coeffs = build_function(coeffs_matrix, u0, t0; expression = Val(false), cse = true)
+        # Wrap to return array of TaylorScalars
+        jet = (
+            (u0_val, t0_val) -> begin
+                coeffs_out = jet_coeffs[1](u0_val, t0_val)
+                return [TaylorScalar(Tuple(coeffs_out[i, :])) for i in 1:n]
+            end,
+            (out, u0_val, t0_val) -> begin
+                coeffs_out = jet_coeffs[2](
+                    similar(coeffs_matrix, eltype(u0_val)), u0_val, t0_val
+                )
+                for i in 1:n
+                    out[i] = TaylorScalar(Tuple(coeffs_out[i, :]))
+                end
+                return out
+            end,
         )
-
-        group = [fill(1, length(orders));
-                 fill(2, length(orders))]
-        barplot!(ax, [orders; orders], [raw; optimized],
-            dodge = group,
-            color = colors[group])
-        elements = [PolyElement(polycolor = colors[i]) for i in 1:2]
-        Legend(f[1, 2], elements, ["Naive", "Simplified"], "Groups")
-        f
+    else
+        # Fallback (shouldn't happen normally)
+        jet = build_function(u, u0, t0; expression = Val(false), cse = true)
     end
-    save("simplification_effect.png", f)
+
+    if !haskey(JET_CACHE, f)
+        JET_CACHE[f] = []
+    end
+    push!(JET_CACHE[f], (order, p, jet))
+    return jet
 end
 
-plot_simplification_effect()
-
-@taylorize function pcr3bp!(dq, q, param, t)
-    local μ = param[1]
-    local onemμ = 1 - μ
-    x1 = q[1]-μ
-    x1sq = x1^2
-    y = q[2]
-    ysq = y^2
-    r1_1p5 = (x1sq+ysq)^1.5
-    x2 = q[1]+onemμ
-    x2sq = x2^2
-    r2_1p5 = (x2sq+ysq)^1.5
-    dq[1] = q[3] + q[2]
-    dq[2] = q[4] - q[1]
-    dq[3] = (-((onemμ*x1)/r1_1p5) - ((μ*x2)/r2_1p5)) + q[4]
-    dq[4] = (-((onemμ*y )/r1_1p5) - ((μ*y )/r2_1p5)) - q[3]
-    return nothing
+function build_propagator(f::ODEFunction{iip}, p, coeffs::NTuple{P, Float64}, length = nothing) where {P, iip}
+    f = unwrapped_f(f)
+    return build_propagator(f, Val{iip}(), p, coeffs, length)
 end
 
-V(x, y) = - (1-μ)/sqrt((x-μ)^2+y^2) - μ/sqrt((x+1-μ)^2+y^2)
-H(x, y, px, py) = (px^2+py^2)/2 - (x*py-y*px) + V(x, y)
-H(x) = H(x...)
-t0 = 0.0
-μ = 0.01
-J0 = -1.58
-function py!(q0, J0)
-    @assert iszero(q0[2]) && iszero(q0[3]) # q0[2] and q0[3] have to be equal to zero
-    q0[4] = q0[1] + sqrt( q0[1]^2-2( V(q0[1], q0[2])-J0 ) )
-    nothing
-end
-q0 = [-0.8, 0.0, 0.0, 0.0]
-py!(q0, J0)
-q0
-tspan = (0.0, 2000.0)
-p = [μ]
-prob = ODEProblem(pcr3bp!, q0, tspan, p)
-raw = Float64[]
-optimized = Float64[]
-orders = [4, 6, 8, 10, 12]
-ti_raw = Float64[]
-ti_optimized = Float64[]
-for P in orders
-    raw_time = @belapsed jetcoeffs($prob.f, $prob.u0, $prob.p, $t0, Val($P))
-    fast_oop, fast_iip = build_jetcoeffs(prob.f, prob.p, Val(P), length(prob.u0))
-    optimized_time = @belapsed $fast_oop($prob.u0, $t0)
-    push!(raw, raw_time)
-    push!(optimized, optimized_time)
-    # TI
-    tt = Taylor1(0.0, P)
-    q = [Taylor1(x, P) for x in q0]
-    dq = [Taylor1(0.0, P) for x in q0]
-    qaux = [Taylor1(0.0, P) for x in q0]
-    a = TaylorIntegration._allocate_jetcoeffs!(Val(pcr3bp!), tt, q, dq, p)
-    ti_time = @belapsed TaylorIntegration.jetcoeffs!($pcr3bp!, $tt, $q, $dq, $qaux, $p)
-    ti_optimized_time = @belapsed TaylorIntegration.jetcoeffs!(Val(pcr3bp!), $tt, $q, $dq, $p, $a)
-    push!(ti_raw, ti_time)
-    push!(ti_optimized, ti_optimized_time)
+function build_propagator(f, ::Val{iip}, p, coeffs::NTuple{P, Float64}, length = nothing) where {P, iip}
+    @variables t0::Real dt::Real
+    u0 = isnothing(length) ? Symbolics.variable(:u0) : Symbolics.variables(:u0, 1:length)
+    if iip
+        f0 = similar(u0)
+        f(f0, u0, p, t0)
+    else
+        f0 = f(u0, p, t0)
+    end
+    u = TaylorDiff.make_seed(u0, f0, Val(1))
+    for index in 2:P
+        t = TaylorScalar{index - 1}(t0, one(t0))
+        if iip
+            fu = similar(u)
+            f(fu, u, p, t)
+        else
+            fu = f(u, p, t)
+        end
+        d = get_coefficient(fu, index - 1) / index
+        u = append_coefficient(u, d)
+    end
+    ut = eval_taylor_polynomial(u, (1., coeffs...), dt)
+    propagator = build_function(ut, u0, t0, dt; expression = Val(false), cse = true)
+    jacobian = Symbolics.jacobian(ut, u0)
+    d_propagator = build_function(jacobian, u0, t0, dt; expression = Val(false), cse = true)
+    return propagator, d_propagator
 end
 
-colors = Makie.wong_colors()
-f = begin
-    f = Figure(resolution = (700, 400))
-    ax = Axis(f[1, 1],
-        xlabel = "Order",
-        ylabel = "Time for computing Taylor polynomial (s)",
-        title = "Effect of Symbolic Simplification on Computing Taylor Polynomial",
-        xticks = orders,
-        yscale = log10
-    )
-
-    group = [fill(1, length(orders));
-                fill(2, length(orders));
-                fill(3, length(orders));
-                fill(4, length(orders))]
-    barplot!(ax, [orders; orders; orders; orders], [raw; optimized; ti_raw; ti_optimized],
-        dodge = group,
-        color = colors[group])
-    elements = [PolyElement(polycolor = colors[i]) for i in 1:4]
-    Legend(f[1, 2], elements, ["Naive", "Simplified", "TI Naive", "TI Simplified"], "Groups")
-    f
-end
-save("simplification_with_ti.png", f)
+# Evaluate polynomial for scalar TaylorScalar (returns scalar)
+@inline eval_taylor_polynomial(u::TaylorScalar, coeffs, dt) = evalpoly(dt, map(*, coeffs, TaylorDiff.flatten(u)))
+# Evaluate polynomial for array of TaylorScalars (returns array)
+@inline eval_taylor_polynomial(us::AbstractArray, coeffs, dt) = map(x -> eval_taylor_polynomial(x, coeffs, dt), us)
